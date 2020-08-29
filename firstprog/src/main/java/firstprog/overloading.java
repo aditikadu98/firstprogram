18f1d3626c14f72fb0acc032e4339c63391d62dd
Aditi <Aditi@Vaidehi-Kadu>
2020-08-29 09:12:02 +0530

    hello

---
+++ firstprog/src/main/java/firstprog/overloading.java
@@ -0,0 +1,8 @@
+package firstprog;
+
+public class overloading {
+
+    public static void main(String args[]) {
+    	System.out.println("my first program");
+    }
+}
---
+++ firstprog/src/test/java/firstprogtest/testfirst.java
@@ -0,0 +1,10 @@
+package firstprogtest;
+
+public class testfirst {
+
+	public static void main(String[] args) {
+		System.out.println("my first test");
+
+	}
+
+}
